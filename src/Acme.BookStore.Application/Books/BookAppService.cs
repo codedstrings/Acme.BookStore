bade592053f4a46412a3db842228efd95a00b2c0
Mridhul M L <mridhulml@gmail.com>
2024-03-21 01:03:20 +0530

    add: override functions, CreateBookSample()

--- src/Acme.BookStore.Application/Books/BookAppService.cs
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Account;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
@@ -14,8 +16,46 @@ public class BookAppService :
         CreateUpdateBookDto>, //Used to create/update a book
     IBookAppService //implement the IBookAppService
 {
-    public BookAppService(IRepository<Book, Guid> repository)
-        : base(repository)
+    //dependency injection
+    private readonly IRepository<Book> _bookRepository; 
+    public BookAppService(IRepository<Book, Guid> bookRepository)
+        : base(bookRepository)
+    {
+        _bookRepository = bookRepository;
+    }
+
+    public override Task<BookDto> CreateAsync(CreateUpdateBookDto input)
+    {
+        input.Name = input.Name + "Mridhul";
+        return base.CreateAsync(input);
+    }
+
+    public override Task<BookDto> GetAsync(Guid id)
+    {
+        return base.GetAsync(id);
+    }
+
+    public override Task<PagedResultDto<BookDto>> GetListAsync(PagedAndSortedResultRequestDto input)
+    {
+        return base.GetListAsync(input);
+    }
+    
+    public void CreateBookSample(CreateUpdateBookDto input)
+    {
+        //manual mapping
+        Book bookEntity = new Book() 
+        {
+            Name = input.Name,
+            Type=input.Type,
+            PublishDate=input.PublishDate,
+            Price=input.Price,
+        };
+
+        //automapping
+        Book bookEntity2 = ObjectMapper.Map<CreateUpdateBookDto, Book>(input);
+        _bookRepository.InsertAsync(bookEntity);
+    }
+    public void Hello()
     {
 
     }
